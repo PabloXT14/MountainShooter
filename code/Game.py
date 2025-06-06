6c8219415a0ca45c1286fcb7e3d79526bc89c44d
Pablo Alan <pabloxt14@gmail.com>
2025-06-06 15:31:52 -0300

    feat: migrate code from main to Game

--- code/Game.py
+++ code/Game.py
@@ -1,6 +1,27 @@
+import pygame
+
+
 class Game:
     def __init__(self):
         self.window = None
 
     def run(self):
-        pass
+        # Initialize pygame
+        pygame.init()
+
+        # Screen
+        width = 800
+        height = 600
+
+        screen = pygame.display.set_mode(size=(width, height))
+        pygame.display.set_caption("Mountain Shooter")
+
+        running = True
+
+        while running:
+            # Events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close the window
+                    quit()  # Close the program
+                    running = False  # Stop the loop
